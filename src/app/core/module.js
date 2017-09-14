e07d050971ac447b2ffbc03db4a9f1357b161675
Marcus Vinicius <vmvini@hotmail.com>
2017-09-14 05:32:59 -0300

    search bar example

--- src/app/core/module.js
+++ src/app/core/module.js
@@ -6,6 +6,7 @@ require('angular-ui-router');
 require('angular-animate');
 require('angular-aria');
 require('angular-material');
+require('angular-material/angular-material.scss');
 
 //local modules
 require('./../screens/start-screen/module');
