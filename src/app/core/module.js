0e7b33b4383857173086878bdb8251d337ab70a6
Marcus Vinicius <vmvini@hotmail.com>
2017-09-14 04:54:46 -0300

    angular material added

--- src/app/core/module.js
+++ src/app/core/module.js
@@ -3,12 +3,15 @@
 //external modules
 require('angular');
 require('angular-ui-router');
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
 
 //local modules
 require('./../screens/start-screen/module');
 
 angular
-    .module('servesom', ['ui.router', 'start-screen']);
+    .module('servesom', ['ui.router', 'start-screen', 'ngMaterial']);
 
 //module internal components
 require('./routeConfig.js');
